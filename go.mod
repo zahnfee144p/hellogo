03537caf08afa82812b2086d730037d6dd99fef7
Oscar Fritzsche <oscar.fritzsche@dart-racing.de>
2025-02-02 20:37:52 +0100

    testing how packages and imports work

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/zahnfee144p/hellogo
 
 go 1.23.5
+
+replace github.com/zahnfee144p/mystrings v0.0.0 => ../mystrings
+
+require (
+    github.com/zahnfee144p/mystrings v0.0.0
+)
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+    "github.com/zahnfee144p/mystrings"
+)
+
+
+func main() {
+    fmt.Println("hello world")
+    fmt.Println(mystrings.Reverse("hello world"))
+}
